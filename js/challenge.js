833e5fe50caa5d0ac403c663236d2d24650aba72
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-31 02:20:49 +0300

    complete assignment

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,14 @@
 let counter = document.getElementById("counter");
 let counterNumber = Number(counter.innerHTML);
 const plus = document.getElementById("plus");
+const heart = document.getElementById("heart");
 const minus = document.getElementById("minus");
 let pause = document.getElementById("pause");
+const submit = document.getElementById("submit");
+const likes = document.getElementById("likes");
 let interval;
+let commentForm = document.getElementById("comment-form");
+
 
 
 function addCounter() {
@@ -28,16 +33,47 @@ function pauseIt() {
     if (pause.innerHTML === "resume") {
         pause.innerHTML = "pause";
         automatic();
-        
+        plus.disabled = false;
+        minus.disabled = false;
+        submit.disabled = false;
+        heart.disabled = false;
+
     } else {
         pause.innerHTML = "resume";
         stopInterval();
+        plus.disabled = true;
+        minus.disabled = true;
+        submit.disabled = true;
+        heart.disabled = false;
+
     }
 }
+
+function handleComments(event){
+    event.preventDefault();
+    let input = document.getElementById("comment-input");
+    let ul = document.getElementById("comments");
+    let li = document.createElement("li");
+    let val = input.value;
+    li.innerHTML = val;
+    ul.append(li)
+    commentForm.reset();
+    
+}
+// function handleLikes(){
+//     let li = document.createElement("li");
+//     for (let count of counterNumber){
+//         if (count)
+//     }
+
+//     likes.appendChild(li)
+// }
 
 automatic();
 pause.addEventListener("click", pauseIt);
 plus.addEventListener("click", addCounter);
 minus.addEventListener("click", minusCounter)
+commentForm.addEventListener("submit", handleComments)
+// heart.addEventListener("click", handleLikes);
 
 
